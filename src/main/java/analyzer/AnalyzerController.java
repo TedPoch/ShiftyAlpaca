ccf67b1f451cf1aec8fbc4ae252fbdb8e3932163
tedpoch <ted.pochmara@hotmail.com>
2018-08-30 11:09:13 -0600

    initial commit

---
+++ src/main/java/analyzer/AnalyzerController.java
@@ -0,0 +1,16 @@
+package analyzer;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AnalyzerController {
+
+  @GetMapping("/analyzer")
+  public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+    model.addAttribute("name", name);
+    return "greeting";
+  }
+}
